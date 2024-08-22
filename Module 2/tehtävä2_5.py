171b966ac217555eec5c49831af0d5903e9d49ae
Elias Rinne <elias1rinne@gmail.com>
2024-08-22 20:22:30 +0300

    päivitykset 4_3

--- Module 2/tehtävä2_5.py
+++ Module 2/tehtävä2_5.py
@@ -1,9 +1,9 @@
 leiviska = int(input("Anna leiviskät: "))
 naula = int(input("Anna naula: "))
 luodit = float(input("Anna luodit: "))
-print(f"Anna leiviskät\n{leiviska}")
-print(f"Anna naula\n{naula}")
-print(f"Anna luodit\n{luodit}")
+print(f"Anna leiviskät.\n{leiviska}")
+print(f"Anna naula.\n{naula}")
+print(f"Anna luodit.\n{luodit}")
 grammoina = ((naula*32)+(leiviska*20*32)+(luodit))*13.3
 kilogrammoina = int(grammoina/1000)
 tulos1 = kilogrammoina
