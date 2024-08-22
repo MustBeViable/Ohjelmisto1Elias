62f1ad8089d681be6725eb9ac283345b444b95a9
Elias Rinne <elias1rinne@gmail.com>
2024-08-22 20:45:10 +0300

    päivitykset 4_4

--- Module 4/tehtävä4_4.py
+++ Module 4/tehtävä4_4.py
@@ -1,16 +1,12 @@
 import random
-num1 = int(input("Arvaa luku: "))
-num2 = int(9)
-#num2 = random.randint(1,10)
+num1 = int(input("Arvaa numero: "))
+num2 = random.randint(1,10)
 while num1 != num2:
-    if int(num1) < int(num2):
-        print("Liian pieni arvaus.")
-        num1 = int(input("Arvaa luku: "))
-        #print("t1")
-    if float(num1) > float(num1):
-        print("Liian suuri arvaus.")
-        num1 = int(input("Arvaa luku: "))
-        #print("t2")
-    if int(num1) == int(num2):
-        #print("t3")
-        print("Oikein.")
+    if num1 < num2:
+        print("Liia pieni")
+        num1 = int(input("Arvaa numero: "))
+    elif num1 > num2:
+        print("liia iso")
+        num1 = int(input("Arvaa numero: "))
+else:
+    print("oikein")
