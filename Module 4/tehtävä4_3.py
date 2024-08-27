521dbbec6eae2719433bbefbff8d1c9d133b8fb7
Elias Rinne <elias1rinne@gmail.com>
2024-08-27 10:37:50 +0300

    patch notes: - testi rivit pois - lisätty printti jos käyttäjä ei syötä ekaakaan lukua

--- Module 4/tehtävä4_3.py
+++ Module 4/tehtävä4_3.py
@@ -1,22 +1,15 @@
 num = input("Anna numero: ")
 if not num:
-    print("t1")
+    print("Ei ole suurinta eikä pienintä lukua.")
 else:
     float(num)
-    print("t2")
 nummax = num
 nummin = num
-print("t3")
 while num:
-    print("t4")
     if float(num) > float(nummax):
         nummax = num
-        print("t5")
     elif float(num) < float(nummin):
         nummin = num
-        print("t6")
-    print("t7")
     num = input("Anna numero: ")
     if not num:
         print(f"Pienin numero on {nummin} ja suurin numero on {nummax}")
-        print("testi vika")
