98d41f4ff65368a1e34f4ab5b7bf12e2d0435507
Elias Rinne <elias1rinne@gmail.com>
2024-09-03 10:18:53 +0300

    Lisäsin breakin, jos käyttäjä haluaakin lopettaa ennen, kun syöttänyt 5 nimeä

--- Module 5/tehtävä5_3.py
+++ Module 5/tehtävä5_3.py
@@ -4,10 +4,23 @@
 #Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten, että jako menee tasan.
 #Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
 num = int(input("Enter a number: "))
-for i in range(2, (num//2)+1):
-    if num % i == 0:
-        print("")
-        break
+if num > 1:
+    print("t0")
+    if num != 2 or num != 3:
+        print("t1")
+        for i in range(2, int((num//2)+1)):
+            print("t2")
+            if num % i == 0:
+                print("t3")
+                print("Ei alkuluku")
+                break
+            else:
+                print("t4")
+                print("Alkuluku")
+                break
     else:
-        print("Alku")
-        break
+        print("t5")
+        print("Alkuluku")
+else:
+    print("t5")
+    print("Ei alkuluku")
