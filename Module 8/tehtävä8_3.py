54db0fa18d55f926ecc57f30446d9fcb1d7e90a7
Elias Rinne <elias1rinne@gmail.com>
2024-09-03 20:08:33 +0300

    tehtävän 8_3 tehtäväannon poisto koodin seasta ja turhien printtien poisto lisäksi hienosäätöä

--- Module 8/tehtävä8_3.py
+++ Module 8/tehtävä8_3.py
@@ -9,9 +9,6 @@ yhteys = mysql.connector.connect(
          autocommit=True,
          collation="utf8mb4_general_ci"
          )
-#Kirjoita ohjelma, joka kysyy käyttäjältä kahden lentokentän ICAO-koodit. Ohjelma ilmoittaa lentokenttien välisen
-# etäisyyden kilometreinä. Laskenta perustuu tietokannasta haettuihin koordinaatteihin. Laske etäisyys
-# geopy-kirjaston avulla
 def cordinate(code):
     sql = (f" select latitude_deg, longitude_deg "
            f" from airport "
@@ -19,14 +16,9 @@ def cordinate(code):
     kursori = yhteys.cursor()
     kursori.execute(sql)
     result = kursori.fetchall()
-#    print(result)
     return result
-#    print(type(result))
-#    print(f"{result[0][0]}, {result[0][1]}")
-ICAO1 = input("A: ")
-#cordinate(ICAO1)
-ICAO2 = input("B: ")
-#cordinate(ICAO2)
+ICAO1 = input("Insert first ICAO code: ")
+ICAO2 = input("Insert second ICAO code: ")
 airport1 = cordinate(ICAO1)
 airport2 = cordinate(ICAO2)
 dist = distance.distance(airport1, airport2).km
