5ffbc0e4236ca6ad97e5128e759e80528c6a7ded
MustBeViable <elias1rinne@gmail.com>
2024-10-22 17:23:19 +0300

    twhtävän 10_1 palautus

---
+++ Module 10/tehtävä10_2.py
@@ -0,0 +1,49 @@
+#Jatka edellisen tehtävän ohjelmaa siten, että teet Talo-luokan. Talon alustajaparametreina annetaan alimman ja ylimmän
+# kerroksen numero sekä hissien lukumäärä. Talon luonnin yhteydessä talo luo tarvittavan määrän hissejä. Hissien lista
+# tallennetaan talon ominaisuutena. Kirjoita taloon metodi aja_hissiä, joka saa parametreinaan hissin numeron ja
+#kohdekerroksen. Kirjoita pääohjelmaan lauseet talon luomiseksi ja talon hisseillä ajelemiseksi.
+
+class Elevator:
+    def __init__(self, lowest, highest):
+        self.lowest = lowest
+        self.highest = highest
+        self.floor = self.lowest
+    def move_to(self, floor):
+        while floor < self.floor:
+            self.move_down()
+        while floor > self.floor:
+            self.move_up()
+        while self.floor != self.lowest:
+            if floor > self.lowest:
+                self.move_down()
+        else:
+            print(f"Hissi on kerroksessa {self.floor}")
+    def move_up(self):
+        if self.floor >= self.highest:
+            self.floor = self.highest
+            print(f"Hissi on kerroksessa {self.floor}")
+        else:
+            print(f"Hissi on kerroksessa {self.floor}")
+            self.floor += 1
+    def move_down(self):
+        if self.floor <= self.lowest:
+            self.floor = self.lowest
+            print(f"Hissi on kerroksessa {self.floor}")
+        else:
+            print(f"Hissi on kerroksessa {self.floor}")
+            self.floor -= 1
+
+class Building:
+    def __init__(self, lowest, highest, elevator_count):
+        self.elevator = []
+        self.highest = highest
+        self.lowest = lowest
+        self.elevator_count = elevator_count
+        for i in range(elevator_count):
+            elevator = Elevator(lowest, highest)
+            self.elevator.append(elevator)
+    def drive_elevators(self, elevator_number, floor):
+        Elevator.move_to(self.elevator[elevator_number-1], floor)
+kasper = Building(1,10,2)
+kasper.drive_elevators(1,5)
+kasper.drive_elevators(2,6)
